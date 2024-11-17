bec5d6c363ab98dbe44ff96c9cbb8d3579dfa4b4
pradertamas <pradert@gmail.com>
2024-11-17 17:08:56 +0100

    v03

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ print("Ez egy könytarkezelő program")
 
 x = 0
 
-while x != 1 and x != 2 and x != 3:
+x = None  # Kezdeti érték
+while x not in [1, 2, 3]:  # Csak akkor lép tovább, ha 1, 2, vagy 3
     print("1. Adminisztráció")
     print("2. Böngészés")
     print("3. Kilépés")
-    x = int(input('Válassz a fenti menüpontokból:'))
+    try:
+        x = int(input('Válassz a fenti menüpontokból: '))
+        if x not in [1, 2, 3]:
+            print("Érvénytelen választás. Próbáld újra!")
+    except ValueError:
+        print("Kérlek, számot adj meg!")
+
 
 if x == 1:
     print ("Beléptél az Admin menübe")
 
 if x == 2:
     print ("Beléptél a Böngészés menübe")
+
+if x == 3:
+    print("Kiléptél a programból!")

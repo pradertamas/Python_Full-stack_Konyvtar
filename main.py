7e592a72da280b5d5ed7795bf1ca243c6d3e0c3d
pradertamas <pradert@gmail.com>
2024-11-17 15:46:53 +0100

    main_v01

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Ez egy könytarkezelő program")
+print("1. Adminisztráció")
+print("2. Böngészés")
+print("3. Kilépés")
+
+x = input('Válassz a fenti menüpontokból:')
+print('A választott menüpont: ' + x)

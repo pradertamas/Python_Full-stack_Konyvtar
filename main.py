badcd45cedc7724aac60b75f69ee4ae39911229c
pradertamas <pradert@gmail.com>
2024-11-17 17:37:29 +0100

    v04

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
 print("Ez egy könytarkezelő program")
 
-x = 0
-
 x = None
+
 while x not in [1, 2, 3]:
-    print("1. Adminisztráció")
-    print("2. Böngészés")
-    print("3. Kilépés")
-    try:
-        x = int(input('Válassz a fenti menüpontokból: '))
-        if x not in [1, 2, 3]:
-            print("Érvénytelen választás. Próbáld újra!")
-    except ValueError:
-        print("Kérlek, számot adj meg!")
+
+    x = None
+    while x not in [1, 2, 3]:
+        print("1. Adminisztráció")
+        print("2. Böngészés")
+        print("3. Kilépés")
+        try:
+            x = int(input('Válassz a fenti menüpontokból: '))
+            if x not in [1, 2, 3]:
+                print("Érvénytelen választás. Próbáld újra!")
+        except ValueError:
+            print("Kérlek, számot adj meg!")
 
 
-if x == 1:
-    print ("Beléptél az Admin menübe")
+    if x == 1:
+        x = None
+        print ("Beléptél az Admin menübe")
+        y = None
+        while y not in [1, 2, 3, 4]:
+            print("1. Új könyv felvétele")
+            print("2. Könyvtári könyvek lekérdezései")
+            print("3. Állományból törlés")
+            print("4. Vissza a főmenübe")
+            try:
+                y = int(input('Válassz a fenti menüpontokból: '))
+                if y not in [1, 2, 3, 4]:
+                    print("Érvénytelen választás. Próbáld újra!")
+            except ValueError:
+                print("Kérlek, számot adj meg!")
+        
+        if y == 1:
+            y = None
+            print ("Beléptél az Új könyv felvétele menübe")
+
+        if y == 2:
+            y = None
+            print("Beléptél az Könyvtári könyvek lekérdezései menübe")
+
+        if y == 3:
+            y = None
+            print("Beléptél az Állományból törlés menübe")
+
+        if y == 4:
+            y = None
+            print("Visszaléptél a Főmenűbe!")
+
+
 
 if x == 2:
+    x = None
     print ("Beléptél a Böngészés menübe")
 
 if x == 3:
+    x = None
     print("Kiléptél a programból!")

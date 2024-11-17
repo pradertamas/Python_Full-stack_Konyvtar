d1d9be7e3a77fca2bec15efc44143a96e58c3554
pradertamas <pradert@gmail.com>
2024-11-17 16:09:50 +0100

    v02

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 print("Ez egy könytarkezelő program")
-print("1. Adminisztráció")
-print("2. Böngészés")
-print("3. Kilépés")
 
-x = input('Válassz a fenti menüpontokból:')
-print('A választott menüpont: ' + x)
+x = 0
+
+while x != 1 and x != 2 and x != 3:
+    print("1. Adminisztráció")
+    print("2. Böngészés")
+    print("3. Kilépés")
+    x = int(input('Válassz a fenti menüpontokból:'))
+
+if x == 1:
+    print ("Beléptél az Admin menübe")
+
+if x == 2:
+    print ("Beléptél a Böngészés menübe")
